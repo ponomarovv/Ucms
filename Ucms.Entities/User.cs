ca3d4777bfcb57f93d455933738bf45bd2bc96de
Ponomarov Oleksii <testdrive819@gmail.com>
2023-10-27 22:27:47 +0300

    add entities project

---
+++ Ucms.Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Ucms.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+    public DateTime Birthday { get; set; }
+    public DateTime HiredDate { get; set; }
+}
