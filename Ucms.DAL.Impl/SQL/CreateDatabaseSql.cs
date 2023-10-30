5b1bc1c6a75356b1c1c33020d47ead93d3aa8d5b
Ponomarov Oleksii <testdrive819@gmail.com>
2023-10-30 22:06:28 +0200

    add create db template sql

---
+++ Ucms.DAL.Impl/SQL/CreateDatabaseSql.cs
@@ -0,0 +1,8 @@
+﻿namespace Ucms.DAL.Impl.SQL;
+
+public class CreateDatabaseSql
+{
+    public static string CreateDatabase { get; set; } = $"";
+    public static string CreateTables { get; set; } = $"";
+    public static string SeedData { get; set; } = $"SELECT * FROM USERS";
+}
