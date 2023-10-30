96734d912bbf45d1251af0312a8f9e65e3b0fafc
Ponomarov Oleksii <testdrive819@gmail.com>
2023-10-30 22:04:58 +0200

    add basic sql commands

---
+++ Ucms.DAL.Impl/SQL/DepartmentsSql.cs
@@ -0,0 +1,6 @@
+﻿namespace Ucms.DAL.Impl.SQL;
+
+public class DepartmentsSql
+{
+    public static string GetAll { get; set; } = $"SELECT * FROM DEPARTMENTS";
+}
---
+++ Ucms.DAL.Impl/SQL/UsersSql.cs
@@ -0,0 +1,6 @@
+﻿namespace Ucms.DAL.Impl.SQL;
+
+public class UsersSql
+{
+    public static string GetAll { get; set; } = $"SELECT * FROM USERS";
+}
