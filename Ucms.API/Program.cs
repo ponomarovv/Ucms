6a096ec559eed738906f22f2558030ddf38b8a47
Ponomarov Oleksii <testdrive819@gmail.com>
2023-10-31 20:18:02 +0200

    create db sql implemented.

--- Ucms.API/Program.cs
+++ Ucms.API/Program.cs
@@ -1,6 +1,24 @@
+using Ucms.DAL.Impl;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+// Add secrets configuration
+builder.Configuration.AddUserSecrets<Program>();
+
+
 // Add services to the container.
+builder.Services.AddSingleton<DatabaseSeeder>();
+builder.Services.InstallRepositories();
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var serviceProvider = builder.Services.BuildServiceProvider();
+var databaseSeeder = serviceProvider.GetRequiredService<DatabaseSeeder>();
+databaseSeeder.SeedDatabase();
+// builder.Services.SeedData();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Ucms.DAL.Impl/DataSeeder.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ucms.DAL.Impl;
+
+public class DatabaseSeeder
+{
+    private readonly IConfiguration _configuration;
+
+    public DatabaseSeeder(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void SeedDatabase()
+    {
+        string createDbConnectionString = _configuration.GetConnectionString("CreateDBConnection");
+
+        CreateDatabase(createDbConnectionString);
+    }
+
+
+    private void CreateDatabase(string connectionString)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string databaseName = "UcmsDB";
+            string createDbQuery =
+                $"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = '{databaseName}')" +
+                $"BEGIN" +
+                $"   CREATE DATABASE {databaseName};" +
+                $"END;";
+
+            using (SqlCommand command = new SqlCommand(createDbQuery, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
